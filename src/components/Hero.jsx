1a2a6776dfc7f33f51180bb8a6aa4c9d7525092f
Kamva-pro <kamvelihlefatman@gmail.com>
2024-10-28 20:24:24 +0200

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
           <p  
           className={`${styles.heroSubText} 
           mt-2 text-white-100`} >
-          I build awesome websites and more
+          I build websites and apps
           </p>
         </div>
       </div>
