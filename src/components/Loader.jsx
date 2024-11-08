bd7344f8869c52a96f8599ccbee002e008312204
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-08 14:31:05 +0200

    fixed loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -7,11 +7,13 @@ const Loader = () => {
   return (
     <Html center>
       <div className="loader-container">
+        {/* Rotating circles */}
         <div className="circle-loader">
           <div className="circle" />
           <div className="circle" />
           <div className="circle" />
         </div>
+        {/* Loading text with progress */}
         <p className="loader-text">
           {progress.toFixed(2)}%
         </p>
