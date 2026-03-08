4f691adaeb6229656ff41996711720599b209857
Kamva-pro <kamvelihlefatman@gmail.com>
2026-03-08 15:47:33 +0200

    Updates

--- assets/About-e306543e.js
+++
@@ -1,6 +0,0 @@
-import{j as s,F as o,a as e,s as c}from"./index-49eb1c55.js";import{_ as d}from"./tilt-72b19599.js";import{s as r}from"./styles-bea25d13.js";import{S as m,m as n,t as p,f as i}from"./SectionWrapper-425e85a7.js";const x=({index:a,title:t,icon:l})=>e(d,{className:"xs:w-[250px] w-full",children:e(n.div,{variants:i("right","spring",.5*a,.75),className:`w-full green-pink-gradient 
-      p-[1px] rounded-[20px] shadow-card`,children:s("div",{options:{max:45,scale:1,speed:450},className:`bg-tertiary rounded-[20px] 
-        py-5 px-12 min-h-[280px] flex
-        justify-evenly items-center flex-col`,children:[e("img",{src:l,alt:t,className:"w-16 h-16 object-contain"}),e("h3",{className:`text-white text-[20px] 
-          font-bold text-center`,children:t})]})})}),h=()=>s(o,{children:[s(n.div,{variants:p(),children:[e("p",{className:r.sectionSubText,children:"Introduction "}),e("h2",{className:r.sectionHeadText,children:"Overview"})]}),e(n.p,{variants:i("","",.1,1),className:`mt-4 text-secondary text-[17px]
-      max-w-3xl leading-[30px]`,children:"I'm a skilled web and mobile developer, with a prime in frontend technologies. I have honed my skills in both the front and backend, establishing myself as a Full-stack developer."}),e("div",{className:"mt-20 flex flex-wrap gap-10",children:c.map((a,t)=>e(x,{index:t,...a},a.title))})]}),g=m(h,"about");export{g as default};
--- assets/Stars-4156bedf.js
+++ assets/Stars-4156bedf.js
@@ -1,4 +1,4 @@
-import{r as a,a as h,j as G}from"./index-49eb1c55.js";import{R as I,_ as y,P as W,u as S,D as d,M as $,V,a as z,C as q}from"./react-three-fiber.esm-e66bf8f8.js";import{P as N}from"./Preload-f88fbf4f.js";const k=()=>parseInt(I.replace(/\D+/g,"")),U=k(),P=U>=154?"opaque_fragment":"output_fragment";class B extends W{constructor(r){super(r),this.onBeforeCompile=(e,n)=>{const{isWebGL2:o}=n.capabilities;e.fragmentShader=e.fragmentShader.replace(`#include <${P}>`,`
+import{r as a,a as h,j as G}from"./index-79a604c0.js";import{R as I,_ as y,P as W,u as S,D as d,M as $,V,a as z,C as q}from"./react-three-fiber.esm-33816510.js";import{P as N}from"./Preload-4931923c.js";const k=()=>parseInt(I.replace(/\D+/g,"")),U=k(),P=U>=154?"opaque_fragment":"output_fragment";class B extends W{constructor(r){super(r),this.onBeforeCompile=(e,n)=>{const{isWebGL2:o}=n.capabilities;e.fragmentShader=e.fragmentShader.replace(`#include <${P}>`,`
         ${o?`#include <${P}>`:`#extension GL_OES_standard_derivatives : enable
 #include <${P}>`}
       vec2 cxy = 2.0 * gl_PointCoord - 1.0;
