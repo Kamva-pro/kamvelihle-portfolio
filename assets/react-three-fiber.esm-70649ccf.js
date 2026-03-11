6e68de2c9ff4ee31bdaebd1245801e6652328d15
kamva-pro <kamvelihlefatman@gmail.com>
2026-03-11 12:59:13 +0200

    Updates

--- assets/react-three-fiber.esm-70649ccf.js
+++ assets/react-three-fiber.esm-70649ccf.js
@@ -1,4 +1,4 @@
-import{r as Ne,h as kg}from"./index-e05bd9bd.js";/**
+import{r as Ne,h as kg}from"./index-8e5b69f5.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
